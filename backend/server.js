cbdf6795ff13b18f23e3bcc2dfc25d500e686333
Your Name <tripathi.akshi11a@gmail.com>
2024-10-06 18:48:14 +0530

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT =5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
