b622e2ef204b30c8f2770cee58dd3cefbcf17301
Akshitripathi <126938167+Akshitripathi@users.noreply.github.com>
2024-10-07 03:08:48 +0530

    Update contactRoutes.js

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { sendContactForm } = require('../controller/contactController');
-
 const router = express.Router();
+const { sendContactForm } = require('../controller/contactController'); // Adjust path as needed
 
-router.post('/', sendContactForm);
+// Define the route to handle contact form submissions
+router.post('/contact', sendContactForm);
 
 module.exports = router;
