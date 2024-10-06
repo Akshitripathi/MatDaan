e27347250bf575d32eee924e25d7592491d437b3
Akshitripathi <126938167+Akshitripathi@users.noreply.github.com>
2024-10-06 23:46:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 require('dotenv').config({ path: '.env' });
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRouter');
+const contactRoutes = require('./routes/contactRoutes');
 const connectDB = require('./middleware/db');
-
 
 const app = express();
 
@@ -11,8 +11,9 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/users', userRoutes);
+app.use('/api/contact', contactRoutes);
 
-const PORT =6000;
+const PORT = 6000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
